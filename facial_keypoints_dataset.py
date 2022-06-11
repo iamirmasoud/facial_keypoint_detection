4322c359069e928675b207694d4cdafbf1b4b170
amir_masoud <amir.masoud.sefidian@gmail.com>
2022-06-11 15:11:38 +0430

    Fix Dataset exploration notebook

---
+++ facial_keypoints_dataset.py
@@ -0,0 +1,38 @@
+from torch.utils.data import Dataset
+
+
+class FacialKeypointsDataset(Dataset):
+    """Face Landmarks dataset."""
+
+    def __init__(self, csv_file, root_dir, transform=None):
+        """
+        Args:
+            csv_file (string): Path to the csv file with annotations.
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.key_pts_frame = pd.read_csv(csv_file, index_col=0)
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.key_pts_frame)
+
+    def __getitem__(self, idx):
+        image_name = os.path.join(self.root_dir, self.key_pts_frame.index[idx])
+
+        image = mpimg.imread(image_name)
+
+        # if image has an alpha color channel, get rid of it
+        if image.shape[2] == 4:
+            image = image[:, :, 0:3]
+
+        key_pts = self.key_pts_frame.iloc[idx, :].values
+        key_pts = key_pts.astype("float").reshape(-1, 2)
+        sample = {"image": image, "keypoints": key_pts}
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
--- models.py
+++ models.py
@@ -8,42 +8,40 @@ import torch.nn.init as I
 
 
 class Net(nn.Module):
-
     def __init__(self):
         super(Net, self).__init__()
-        
+
         ## TODO: Define all the layers of this CNN, the only requirements are:
         ## 1. This network takes in a square (same width and height), grayscale image as input
         ## 2. It ends with a linear layer that represents the keypoints
         ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
-        
+
         # default conv2d: in_channels, out_channels, kernel_size, stride=1, padding=0
         # default maxpool2d: kernel_size, stride=kernel_size, padding=0
-        
-        # input image : 1 x 224 x 224, grayscale squared images
-        
 
-        self.conv1 = nn.Conv2d(1, 32, 4) # 32*(4,4) filter ==> 221*221*32
+        # input image : 1 x 224 x 224, grayscale squared images
+
+        self.conv1 = nn.Conv2d(1, 32, 4)  # 32*(4,4) filter ==> 221*221*32
         self.bn1 = nn.BatchNorm2d(32)
-        self.pool1 = nn.MaxPool2d(2,2) # pool (2,2) ==> 110*110*32
+        self.pool1 = nn.MaxPool2d(2, 2)  # pool (2,2) ==> 110*110*32
         self.dropout1 = nn.Dropout(p=0.1)
 
-        self.conv2 = nn.Conv2d(32, 64, 3) # 64*(3,3) filter ==> 108*108*64
+        self.conv2 = nn.Conv2d(32, 64, 3)  # 64*(3,3) filter ==> 108*108*64
         self.bn2 = nn.BatchNorm2d(64)
-        self.pool2 = nn.MaxPool2d(2,2) # pool (2,2) ==> 54*54*64
+        self.pool2 = nn.MaxPool2d(2, 2)  # pool (2,2) ==> 54*54*64
         self.dropout2 = nn.Dropout(p=0.2)
 
-        self.conv3 = nn.Conv2d(64, 128, 2) # 128*(2,2) filter ==> 53*53*128
+        self.conv3 = nn.Conv2d(64, 128, 2)  # 128*(2,2) filter ==> 53*53*128
         self.bn3 = nn.BatchNorm2d(128)
-        self.pool3 = nn.MaxPool2d(2,2) # pool (2,2) ==> 26*26*128
+        self.pool3 = nn.MaxPool2d(2, 2)  # pool (2,2) ==> 26*26*128
         self.dropout3 = nn.Dropout(p=0.3)
 
-        self.conv4 = nn.Conv2d(128, 256, 1) # 256*(1,1) filter ==> 26*26*256
+        self.conv4 = nn.Conv2d(128, 256, 1)  # 256*(1,1) filter ==> 26*26*256
         self.bn4 = nn.BatchNorm2d(256)
-        self.pool4 = nn.MaxPool2d(2,2) # pool (2,2) ==> 13*13*256
+        self.pool4 = nn.MaxPool2d(2, 2)  # pool (2,2) ==> 13*13*256
         self.dropout4 = nn.Dropout(p=0.4)
 
-        self.fc1 = nn.Linear(13*13*256, 1000)
+        self.fc1 = nn.Linear(13 * 13 * 256, 1000)
         self.bn5 = nn.BatchNorm1d(1000)
         self.dropout5 = nn.Dropout(p=0.5)
 
@@ -52,28 +50,26 @@ class Net(nn.Module):
         self.dropout6 = nn.Dropout(p=0.6)
 
         self.fc3 = nn.Linear(1000, 136)
-        
 
         I.xavier_uniform_(self.fc1.weight.data)
         I.xavier_uniform_(self.fc2.weight.data)
         I.xavier_uniform_(self.fc3.weight.data)
-        
+
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
-      
+
         x = self.dropout1(self.pool1(F.elu(self.bn1(self.conv1(x)))))
         x = self.dropout2(self.pool2(F.elu(self.bn2(self.conv2(x)))))
         x = self.dropout3(self.pool3(F.elu(self.bn3(self.conv3(x)))))
         x = self.dropout4(self.pool4(F.elu(self.bn4(self.conv4(x)))))
 
         # flatten
-        x = x.view(x.size(0),-1)
+        x = x.view(x.size(0), -1)
 
         x = self.dropout5(F.elu(self.bn5(self.fc1(x))))
         x = self.dropout6(F.elu(self.bn6(self.fc2(x))))
         x = self.fc3(x)
 
         return x
-
